8d327fff887af953fef4e761e22e8fe26d63da50
Richard Koubek <koubek.richard@gmail.com>
2024-05-09 18:32:48 +0200

    tipsinput textfield added

--- bt_frontend/src/app/tipsinput/tipsinput.component.ts
+++ bt_frontend/src/app/tipsinput/tipsinput.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tipsinput',
-  templateUrl: './tipsinput.component.html',
+  template: `
+        <h2>Please enter the names</h2>
+        <input type="text" value="Name">
+            `,
   styleUrl: './tipsinput.component.css'
 })
 export class TipsinputComponent {

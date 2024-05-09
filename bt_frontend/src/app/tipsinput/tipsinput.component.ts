80e7bf092f84d18f88b17b7fda6a92bf6bb741d7
Richard Koubek <koubek.richard@gmail.com>
2024-05-09 19:00:44 +0200

    input fields for names and hours added

--- bt_frontend/src/app/tipsinput/tipsinput.component.ts
+++ bt_frontend/src/app/tipsinput/tipsinput.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tipsinput',
   template: `
-        <h2>Please enter the names</h2>
-        <input type="text" value="Name">
+  <h2>Please enter the names</h2>
+  <input id="names" type="text" value="">
+  <label for="names">Names</label>
+  <h2>Please enter the hours worked (eg 1.5)</h2>
+  <input id="hours" type="number" value="">
+  <label for="hours">Hours</label>
             `,
   styleUrl: './tipsinput.component.css'
 })
